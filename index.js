9c8f0740a15083973b19d93bed15ce8810416439
dmillerg <dmillergg@gmail.com>
2021-09-14 16:15:34 -0400

    ajustes

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 // console.log('Hola');
-// const ipc = require('electron').ipcRenderer;
+const ipc = require('electron').ipcRenderer;
 // // var ipc = require('electron');
 // document.getElementById('close').addEventListener('click', () => {
 //     console.log('asdasda');
 //     ipc.send('window-close');
 // });
 
-var windowTopbar = document.createElement('div')
-windowTopbar.style.width = '100%'
-windowTopbar.style.height = '32px'
-windowTopbar.style.backgroundColor = '#000'
-windowTopbar.style.position = 'absolute'
-windowTopbar.style.top = 0
-windowTopbar.style.left = 0
-windowTopbar.style.webkitAppRegion = 'drag'
-document.body.appendChild(windowTopbar)
+
+document.getElementById('close').addEventListener('click', () => {
+    console.log('cerrando app');
+    ipc.send('window-close');
+});
